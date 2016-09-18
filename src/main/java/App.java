56128f8ba952d643ad541cdefe48fce20809d147
mpeedosk <martinpeedosk@gmail.com>
2016-09-18 15:09:05 +0300

    Initial structure

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Martin on 18.09.2016.
+ */
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
