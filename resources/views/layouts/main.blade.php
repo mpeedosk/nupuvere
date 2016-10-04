52c146157c1e5834e61b5839c0433e9dd4cb3a50
mpeedosk <martinpeedosk@gmail.com>
2016-10-04 22:36:22 +0300

    revert back

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,30 +13,42 @@
     <link href="favicon.ico" rel="shortcut icon">
 
 
-
     <!-- Bootstrap Core CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css">
 
 
+@if (App::isLocal())
     <!-- Bootstrap Material Design -->
-        <link rel="stylesheet" type="text/css" href="{{'//css/material/bootstrap-material-design.css'}}">
-        <link rel="stylesheet" type="text/css" href="{{'//css/material/ripples.css'}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/material/bootstrap-material-design.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/material/ripples.css')}}">
 
 
         <!-- Template CSS -->
-        <link rel="stylesheet" href="{{'//css/animate.css'}}">
-        <link rel="stylesheet" href="{{'//css/font-awesome.css'}}">
-        <link rel="stylesheet" href="{{'//css/main.css'}}">
-        <link rel="stylesheet" href="{{'//css/responsive.css'}}">
+        <link rel="stylesheet" href="{{asset('css/animate.css')}}">
+        <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
+        <link rel="stylesheet" href="{{asset('css/main.css')}}">
+        <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
+@else
+    <!-- Bootstrap Material Design -->
+        <link rel="stylesheet" type="text/css" href="{{secure_asset('css/material/bootstrap-material-design.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{secure_asset('css/material/ripples.css')}}">
+
+
+        <!-- Template CSS -->
+        <link rel="stylesheet" href="{{secure_asset('css/animate.css')}}">
+        <link rel="stylesheet" href="{{secure_asset('css/font-awesome.css')}}">
+        <link rel="stylesheet" href="{{secure_asset('css/main.css')}}">
+        <link rel="stylesheet" href="{{secure_asset('css/responsive.css')}}">
+@endif
 
 
 
-    <!-- Google Fonts-->
+
+<!-- Google Fonts-->
     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300" rel="stylesheet" type="text/css">
 
 </head>
 <body>
-
 
 
 <div id="body-bg">
@@ -76,12 +88,35 @@
 </div>
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/SlickNav/1.0.10/jquery.slicknav.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.5.10/js/material.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.5.10/js/ripples.min.js"></script>
+<script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.5.10/js/material.min.js"></script>
+<script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.5.10/js/ripples.min.js"></script>
+@if (App::isLocal())
+    <script type="text/javascript" src="{{asset('js/modernizr.custom.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/scripts.js')}}"></script>
+@else
+    <!-- JS -->
+    {{--<script type="text/javascript" src="{{secure_asset('js/jquery.min.js')}}"></script>--}}
+    {{--<script type="text/javascript" src="{{secure_asset('js/bootstrap.min.js')}}"></script>--}}
+    <!-- Isotope - Portfolio Sorting -->
+    {{--<script type="text/javascript" src="{{secure_asset('js/jquery.isotope.js')}}"></script>--}}
+    <!-- Mobile Menu - Slicknav -->
+    {{--<script type="text/javascript" src="{{secure_asset('js/jquery.slicknav.js')}}"></script>--}}
 
-<script type="text/javascript" src="{{'//js/modernizr.custom.js'}}" ></script>
-<script type="text/javascript" src="{{'//js/scripts.js'}}"></script>
+
+
+    <!-- Animate on Scroll-->
+    {{--<script type="text/javascript" src="{{secure_asset('js/jquery.visible.js')}}" charset="utf-8"></script>--}}
+    {{--<!-- Slimbox2-->--}}
+    {{--<script type="text/javascript" src="{{secure_asset('js/slimbox2.js')}}" charset="utf-8"></script>--}}
+    {{--<!-- Modernizr -->--}}
+    <script type="text/javascript" src="{{secure_asset('js/modernizr.custom.js')}}"></script>
+    <script type="text/javascript" src="{{secure_asset('js/scripts.js')}}"></script>
+@endif
+
 </body>
 </html>
