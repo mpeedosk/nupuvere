d18d9ef2489a8c809d6376ec21d57aa8f8500d14
mpeedosk <martinpeedosk@gmail.com>
2016-11-08 01:37:27 +0200

    submenu when too many categories

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -3,6 +3,9 @@
         <div class="row">
             <ul id="navmenu" class="nav navbar-nav">
                 @foreach(App\Category::getCategories() as $cat )
+                    @if($loop->iteration > 6)
+                        @break
+                    @endif
                     <li style="border-bottom: 4px solid {{$cat->color}}; @if(isset($category)&& $category ==
                         mb_strtolower($cat->name) ) background-color: {{$cat->color}};@endif">
                         <span>{{str_replace('_', ' ', ucfirst($cat->name)) }}</span>
@@ -35,9 +38,58 @@
                                         <em class="pull-right">(10. - 12. kl)</em></a>
                                 </div>
                             </li>
+
                         </ul>
                     </li>
                 @endforeach
+
+                @if(count(App\Category::getCategories()) > 6)
+                    <li class="menu-expand">
+                        <i class="fa fa-bars fa-fw fa-2x" aria-hidden="true"></i>
+                        <ul>
+                            @foreach(App\Category::getCategories() as $cat )
+                                @if($loop->iteration <= 6)
+                                    @continue
+                                @endif
+                                <li style="border-bottom: 4px solid {{$cat->color}}; @if(isset($category)&& $category ==
+                        mb_strtolower($cat->name) ) background-color: {{$cat->color}};@endif">
+                                    <span>{{str_replace('_', ' ', ucfirst($cat->name)) }}</span>
+                                    <ul>
+                                        <li style="border-bottom: 3px solid {{shade4($cat->color)}}">
+                                            <div>
+                                                <a class="withripple"
+                                                   href="/{{str_replace(' ', '_', mb_strtolower($cat->name))}}/avastaja">Avastajad
+                                                    <em class="pull-right">(.. - 2.kl)</em></a>
+                                            </div>
+                                        </li>
+                                        <li style="border-bottom: 3px solid {{shade3($cat->color)}}">
+                                            <div>
+                                                <a class="withripple"
+                                                   href="/{{str_replace(' ', '_', mb_strtolower($cat->name))}}/uurija">Uurijad
+                                                    <em class="pull-right">(3. - 6.kl)</em></a>
+                                            </div>
+                                        </li>
+                                        <li style="border-bottom: 3px solid {{shade1($cat->color)}}">
+                                            <div>
+                                                <a class="withripple"
+                                                   href="/{{str_replace(' ', '_', mb_strtolower($cat->name))}}/teadja">Teadjad
+                                                    <em class="pull-right">(7. - 9. kl)</em></a>
+                                            </div>
+                                        </li>
+                                        <li style="border-bottom: 3px solid {{shade1($cat->color)}}">
+                                            <div>
+                                                <a class="withripple"
+                                                   href="/{{str_replace(' ', '_', mb_strtolower($cat->name))}}/ekspert">Eksperdid
+                                                    <em class="pull-right">(10. - 12. kl)</em></a>
+                                            </div>
+                                        </li>
+
+                                    </ul>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </li>
+                @endif
 
                 <li class="hidden-md hidden-lg">
                     <a href=""> Edetabel</a>
