f6d8d935d8104f1e4f203ffee73315b66b7e0078
unknown <martin.liba@hotmail.com name = libamart>
2016-12-04 14:57:51 +0200

    UI tests for core use cases.

--- tests/UITest.php
+++ tests/UITest.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -49,6 +49,7 @@ class UITest extends TestCase
              ->see('Raske')
              ->dontSee('Telephone');
     }
+    
     public function testSeeExerciseEasy()//kontrollib kas menüü valimine kuvatakse.
     {
         $this->visit('/matemaatika/avastaja')
@@ -69,6 +70,30 @@ class UITest extends TestCase
             ->dontSee('Telephone');
     }
 
+
+    public function testShowExerciseAvastajadEasy()//Kontrollib kas ülesande valimine ja kuvamine toimib õigesti(sisselogitud).
+    {
+        $this->visit('/')
+             ->type('martinliba','login')
+             ->type('parool','password')
+             ->press('Logi sisse')
+             ->visit('/matemaatika/avastaja')
+             ->click('Ülesanne 1')
+             ->see('Ülesanne 1')
+             ->see('Retseptis oli öeldud, et antud kogustega tuleb hea ja õige paksusega kook, kui kasutada')
+             ->see('Vihje');
+    }
+    
+    public function testShowExerciseAvastajadEasyNotLoggedIn()//Kontrollib kas ülesande valimine ja kuvamine toimib õigesti(sisselogimata).
+    {
+        $this->visit('/')
+             ->visit('/matemaatika/avastaja')
+             ->click('Ülesanne 1')
+             ->see('Ülesanne 1')
+             ->see('Retseptis oli öeldud, et antud kogustega tuleb hea ja õige paksusega kook, kui kasutada')
+             ->see('Vihje');
+    }
+
     public function testSolvExerciseAvastajadEasy()//Kontrollib kas ülesande lahendamine kuvatakse õigesti.
     {
         $this->visit('/')
@@ -82,8 +107,61 @@ class UITest extends TestCase
              ->see('Retseptis');
     }
 
+    public function testSkipExerciseAvastajadEasy()//Kontrollib kas ülesande vahelejätmine kuvatakse õigesti(sisselogitud).
+    {
+        $this->visit('/')
+             ->type('martinliba','login')
+             ->type('parool','password')
+             ->press('Logi sisse')
+             ->visit('/matemaatika/avastaja')
+             ->click('Ülesanne 1')
+             ->click('Edasi')
+             ->see('Ülesanne 4')
+             ->see('Aadu, Beedu, Ceedu ja')
+             ->see('vasta');
+    }
+    public function testSkipExerciseAvastajadEasyNotLoggedIn()//Kontrollib kas ülesande vahelejätmine kuvatakse õigesti(sisselogimata).
+    {
+        $this->visit('/')
+             ->visit('/matemaatika/avastaja')
+             ->click('Ülesanne 1')
+             ->click('Edasi')
+             ->see('Ülesanne 4')
+             ->see('Aadu, Beedu, Ceedu ja');
+    }
 
-   public function testAdminAddExercise() // Kontrolliba, kui admin lisab ülesandeid, kas kõik kuvatakse õigesti.
+ public function testSkipExerciseAvastajadEasyLastEX()//Kontrollib, kas ülesande vahelejätmine kuvatakse õigesti, kui tegemist on viimase ülesandega nimekirjas(sisselogitud).
+    {
+        $this->visit('/')
+             ->type('martinliba','login')
+             ->type('parool','password')
+             ->press('Logi sisse')
+             ->visit('/matemaatika/avastaja')
+             ->click('Ülesanne 1')
+             ->click('Edasi')
+             ->see('Ülesanne 4')
+             ->see('Aadu, Beedu, Ceedu ja')
+             ->see('vasta')
+             ->click('Edasi')
+             ->see('Lihtne')
+             ->see('Raske');
+    }
+
+    public function testSkipExerciseAvastajadEasyLastEXNotLoggedIn()//Kontrollib, kas ülesande vahelejätmine kuvatakse õigesti, kui tegemist on viimase ülesandega nimekirjas(sisselogimata).
+    {
+        $this->visit('/')
+             ->visit('/matemaatika/avastaja')
+             ->click('Ülesanne 1')
+             ->click('Edasi')
+             ->see('Ülesanne 4')
+             ->see('Aadu, Beedu, Ceedu ja')
+             ->see('vasta')
+             ->click('Edasi')
+             ->see('Lihtne')
+             ->see('Raske');
+    }
+
+   public function testAdminAddExercise() // Kontrollib, kui admin lisab ülesandeid, kas kõik kuvatakse õigesti.
     {
         $this->visit('/')
              ->type('admin','login')
@@ -104,6 +182,28 @@ class UITest extends TestCase
              ->press('Lisa ülesanne')
              ->visit('/matemaatika/avastaja')
              ->see('testülesanne');
+        }
+
+        public function testAdminUpdateExercise() // Kontrollib, kui admin uuendab ülesandeid, kas kõik kuvatakse õigesti.
+    {
+        $this->visit('/')
+             ->type('admin','login')
+             ->type('parool','password')
+             ->press('Logi sisse')
+             ->see('Admin')
+             ->click('Admin')
+             ->see('Kategooriad')
+             ->click('Ülesanded')
+             ->see('Lisa uus')
+             ->visit('/admin/exercise/edit/1')
+             ->see('Uuenda')
+             ->type('Uuendatud autor','ex_author')
+             ->type('Uuendatud ülesande sisu','ex_content')
+             ->press('Uuenda')
+             ->visit('/matemaatika/avastaja')
+             ->click('Ülesanne 1')
+             ->see('Uuendatud autor')
+             ->see('Uuendatud ülesande sisu');
         }
 
     public function testAdminAddExerciseEmptyTitle()//Kontrollib, kas veateade kuvatakse õigesti, kui ülesande lisamisel pealikiri jääb tühjaks.
@@ -128,6 +228,8 @@ class UITest extends TestCase
              ->see('See tunnus on kohustuslik.');
         }
 
+     
+
     public function testAdminAddExerciseEmptyContent()//Kontrollib, kas veateade kuvatakse õigesti, kui ülesande lisamisel sisu jääb tühjaks.
     {
         $this->visit('/')
@@ -149,4 +251,57 @@ class UITest extends TestCase
              ->press('Lisa ülesanne')
              ->see('See tunnus on kohustuslik.');
         }
+
+        public function testAdminChangeCategory() // Kontrollib, kui admin lisab kategooria, kas kõik kuvatakse õigesti.
+    {
+        $this->visit('/')
+             ->type('admin','login')
+             ->type('parool','password')
+             ->press('Logi sisse')
+             ->see('Admin')
+             ->click('Admin')
+             ->see('Kategooriad')
+             ->click('Kategooriad')
+             ->see('Uus kategooria:')
+             ->type('Testk','name')
+             ->press('Lisa uus')
+             ->see('Testk');
+        }
+
+
+        public function testAdminChangeCategoryOrder() // Kontrollib, kui admin muudab kategooria järjekorda, kas kõik kuvatakse õigesti.
+    {
+        $this->visit('/')
+             ->type('admin','login')
+             ->type('parool','password')
+             ->press('Logi sisse')
+             ->see('Admin')
+             ->click('Admin')
+             ->see('Kategooriad')
+             ->click('Kategooriad')
+             ->see('Uus kategooria:')
+             ->type('Testk','name')
+             ->press('Lisa uus')
+             ->see('Testk')
+              ->type('1','testk')
+             ->type('7','matemaatika')
+             ->press('Salvesta')
+             ->click('Tagasi pealehele')
+             ->see('Testk');
+        }
+
+        public function testAdminBeholdExercises() // Kontrollib, kas admin näeb ülesandeid.
+    {
+        $this->visit('/')
+             ->type('admin','login')
+             ->type('parool','password')
+             ->press('Logi sisse')
+             ->see('Admin')
+             ->click('Admin')
+             ->see('Kategooriad')
+             ->click('Ülesanded')
+             ->see('Lisa uus')
+             ->see('Ülesanded');
+        }
+
 }
